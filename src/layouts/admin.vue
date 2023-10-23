bd6d0e676b68d6e4a266239a08c5cd28d0eb827b
666 <666@senkoo.cn>
2023-10-23 14:49:55 +0800

    fix bug

--- src/layouts/admin.vue
+++ src/layouts/admin.vue
@@ -1,9 +1,28 @@
 <template>
     <a-layout>
-        <a-layout-header>Header</a-layout-header>
-        <a-layout-content>
+        <a-layout-header :style="{ background: token.colorBgContainer }">
+            <div class="flex rn aic"></div>
+        </a-layout-header>
+        <a-layout-content class="layout_content">
             <router-view></router-view>
         </a-layout-content>
-        <a-layout-footer>Footer</a-layout-footer>
     </a-layout>
 </template>
+
+<script setup>
+import { theme } from "ant-design-vue";
+
+const { useToken } = theme;
+const { token } = useToken();
+</script>
+
+<style lang="scss" scoped>
+.layout_content {
+    width: 100vw;
+    max-width: 1200px;
+    margin: 0 auto;
+    min-height: calc(100vh - 64px);
+    box-sizing: border-box;
+    padding: 15px 50px;
+}
+</style>
