60f771b9662d6b36c126cbefe5a7f7c8e12e219e
666 <666@senkoo.cn>
2023-10-23 10:54:11 +0800

    修改内容

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,12 @@
 <template>
-  <router-view></router-view>
+    <a-config-provider>
+        <router-view></router-view>
+    </a-config-provider>
 </template>
+
+<script setup>
+import { useSystemStore } from "@/stores/system.js";
+
+const systemStore = useSystemStore();
+systemStore.changeLocale();
+</script>
--- src/stores/system.js
+++ src/stores/system.js
@@ -1,7 +1,44 @@
 import { defineStore } from "pinia";
+import dayjs from "dayjs";
+import "dayjs/locale/zh-cn";
+import "dayjs/locale/en";
+import zhCN from "ant-design-vue/es/locale/zh_CN";
+import enUS from "ant-design-vue/es/locale/en_US";
+
+/**
+ * @description: 语言列表
+ * @return {*}
+ */
+const locales = {
+    en: {
+        theme: enUS,
+        day: "en",
+    },
+    cn: {
+        theme: zhCN,
+        day: "zh-cn",
+    },
+};
 
 export const useSystemStore = defineStore("system", {
-    state: () => ({}),
-    actions: {},
+    state: () => ({
+        theme: zhCN,
+    }),
+    actions: {
+        /**
+         * @description: 修改语言
+         * @param {*} locale
+         * @return {*}
+         */
+        changeLocale(locale) {
+            if (locale.includes("en")) {
+                this.theme = locales.en.theme;
+                dayjs.locale("en");
+            } else {
+                this.theme = locales.cn.theme;
+                dayjs.locale("zh-cn");
+            }
+        },
+    },
     persist: true,
 });
